ce59304768e9000b64d4956d6d1203f3a4823c0a
shigwst <499640883@qq.com>
2020-07-05 17:45:11 +0800

    阶段三，模块一作业

---
+++ rpc-consumer/src/main/java/com/lagou/boot/ClientBootStrap.java
@@ -0,0 +1,30 @@
+package com.lagou.boot;
+
+import com.lagou.client.RpcConsumer;
+import com.lagou.service.UserService;
+
+
+public class ClientBootStrap {
+
+   // public static  final String providerName="UserService#sayHello#";
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+        RpcConsumer rpcConsumer = new RpcConsumer();
+        UserService proxy = (UserService) rpcConsumer.createProxy(UserService.class);
+
+        while (true){
+            Thread.sleep(2000);
+            proxy.sayHello("are you ok?");
+            System.out.println("已响应");
+        }
+
+
+    }
+
+
+
+
+}
